b15767916401478813bd6b07e88399c7b7f18ccb
kj119 <69178236+kj119@users.noreply.github.com>
2021-03-06 00:11:52 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"<kjiang119@yahoo.com>\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
